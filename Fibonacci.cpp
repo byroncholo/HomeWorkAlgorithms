2cf6dd3352b980dfa23633b36982387f058e6d33
Byron Cholo <99635116+byroncholo@users.noreply.github.com>
2026-02-06 22:38:00 -0500

    Refactor Fibonacci functions for iteration and recursion

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -10,36 +10,40 @@ T(1) = 1
 T(2) = 1
 2.1 Write a C++ program that generates the sequence using iteration.
 2.2 Write a C++ program that generates the sequence using recursion
-
+Solution:
 */
-
-
-
 /* 2.1 Fibonacci with iteration */
 int fibIter(int n ){
-    int Tn1 = 1;
-    int Tn2 = 1;
+    int Fn1 = 1;
+    int Fn2 = 1;
+    int Fn3 = 0;
+
+    if (n == 1) 
+        return Fn1;
+    if (n == 2) 
+        return Fn2;
     for(int i = 3; i <= n; i++){
-        int Tn3 = Tn1 + Tn2; // the next term is generated with the sum of the previous two
-        Tn1 = Tn2; // change the predecesors numbers
-        Tn2 = Tn3; 
+        Fn3 = Fn1 + Fn2; // the next term is generated with the sum of the previous two
+        Fn1 = Fn2; // change the predecesors numbers
+        Fn2 = Fn3; 
     }
-    return Tn2; // return the last term in the sequence
-
-
+    return Fn3; // return the last term in the sequence
 }
+
+
+
 /* 2.2 Fibonacci with recursion*/
 
-int T(int n){
+int F(int n){
     if(n == 1 || n == 2) // base case
         return 1;
     return 
-        T(n-1) + T(n-2); // induction step
+        F(n-1) + F(n-2); // induction step
 }
 
 
 int main(){
     // sample output
-    std::cout << fibIter(10) << std::endl;
-    std::cout << T(10) << std::endl;
+    std::cout << fibIter(6) << std::endl;
+    std::cout << F(6) << std::endl;
 }
